1b133b855c7ea8bd6d1032cc48fe13462b8a0318
João Pedro Bastos <jpedrosantosb@gmail.com>
2023-02-13 23:36:15 -0300

    listas

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 Alura Midi
 
 ![imagealuramidi](https://user-images.githubusercontent.com/69656085/218620552-fa03c628-db87-4d29-a0f2-fc18b8be860e.png)
+
+Nesse curso aprendi a;
+
+- Identificar os papéis de cada tecnologia da Web para construção de páginas
+- Associar o JavaScript com HTML através do DOM
+- Implementar funções com JavaScript
+- Investigar possibilidades de resolução de problemas de código
+- Selecionar operadores lógicos adequados para diferentes situações
+- Produzir soluções simples para páginas dinâmicas
--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play();
-}
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+}
